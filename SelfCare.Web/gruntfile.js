fc55022ed0592e5cd38668f81dd64bfb6ed29c9b
Lilanga Gamage <lilanga@live.com>
2015-11-14 00:01:32 +0530

    grunt task for less compilation

---
+++ SelfCare.Web/gruntfile.js
@@ -0,0 +1,47 @@
+﻿/// <binding AfterBuild='less' ProjectOpened='watch' />
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: 'wwwroot/lib',
+                    layout: 'byComponent',
+                    cleanTargetDir: false
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['content/*.less'],
+                tasks: ['less'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        less: {
+            production: {
+                options: {
+                    cleancss: true
+                },
+                files: {
+                    "content/app.min.css": 'content/app.less'
+                }
+            },
+            development: {
+                options: {
+                    paths: ['content']
+                },
+                files: {
+                    "content/app.css": 'content/app.less'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['bower:install']);
+
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
